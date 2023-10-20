17a5f65a078ae7823e867e9f916e9b703bee5da3
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2023-10-20 16:02:55 +0200

    added some ui elements

---
+++ src/components/Button.tsx
@@ -0,0 +1,41 @@
+// src/components/Button.tsx
+
+import Link from "next/link";
+import React from "react";
+
+interface ButtonProps {
+  href?: string;
+  onClick?: (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => void | Promise<void>;
+  className?: string;
+  children: React.ReactNode;
+  disabled?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  href,
+  onClick,
+  className,
+  children,
+  disabled,
+}) => {
+  return href ? (
+    <Link
+      href={href}
+      className={`rounded-full bg-black/10 px-4 py-3 font-semibold no-underline transition hover:bg-black/20 sm:px-10 ${className}`}
+    >
+      {children}
+    </Link>
+  ) : (
+    <button
+      className={`rounded-full bg-black/10 px-4 py-3 font-semibold no-underline transition hover:bg-black/20 sm:px-10 ${className}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
