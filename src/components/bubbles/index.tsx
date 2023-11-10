0ebb81e55dec33b086f4c3ac78523b7e8993f3c8
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2023-11-10 11:55:21 +0100

    implemented bubbles

--- src/components/bubbles/index.tsx
+++ src/components/bubbles/index.tsx
@@ -1,48 +1,67 @@
-import { useRef, useState } from "react";
-import { getSVGDimensions } from "../editor/utils/getSVGDimensions";
-import { aspectRatios } from "../editor/utils/aspectRatios";
+import { useData } from "~/contexts/DataContext";
+
+interface MappedActivity {
+  dayOfYear: number;
+  intensity: number;
+}
+
+// TODO: Add dropdown for multiple years
 
 export default function Bubbles() {
-  const svgRef = useRef<SVGSVGElement>(null);
+  const { activities } = useData();
 
-  // State hooks
-  const [backgroundColor, setBackgroundColor] = useState("#ffffff");
-  const [strokeColor, setStrokeColor] = useState("#000000");
-  const [isOverlayOpen, setOverlayOpen] = useState(false);
-  const [currentAspectRatio, setCurrentAspectRatio] = useState(
-    aspectRatios[0]!,
-  );
-  const [strokeWidth, setStrokeWidth] = useState(4);
-  const [padding, setPadding] = useState(10);
-  const [selectedActivityIndex, setSelectedActivityIndex] = useState<
-    number | null
-  >(null);
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [isAddModalVisible, setIsAddModalVisible] = useState(false);
+  const numberOfElements = 365;
 
-  const { SVG_WIDTH, SVG_HEIGHT, MAX_ACTIVITIES } =
-    getSVGDimensions(currentAspectRatio);
+  const getDayOfYear = (dateString: string): number => {
+    const date = new Date(dateString);
+    const start = new Date(date.getFullYear(), 0, 0);
+    const diff = date.getTime() - start.getTime();
+    const oneDay = 1000 * 60 * 60 * 24;
+    return Math.floor(diff / oneDay) - 1;
+  };
+
+  const maxMovingTime = Math.max(...activities.map((a) => a.moving_time / 60));
+  const minMovingTime = Math.min(...activities.map((a) => a.moving_time / 60));
+
+  const logIntensity = (value: number, min: number, max: number): number => {
+    const adjustedValue = Math.max(value, 1);
+    return (
+      (Math.log(adjustedValue) - Math.log(min)) /
+      (Math.log(max) - Math.log(min))
+    );
+  };
+
+  const mappedActivities: MappedActivity[] = activities.map((activity) => {
+    const dayOfYear = getDayOfYear(activity.start_date_local);
+    const intensity =
+      logIntensity(activity.moving_time / 60, minMovingTime, maxMovingTime) *
+        80 +
+      20;
+    return { dayOfYear, intensity };
+  });
+
+  const generateElements = (): JSX.Element[] => {
+    const elements: JSX.Element[] = [];
+    for (let i = 0; i < numberOfElements; i++) {
+      const activity = mappedActivities.find((a) => a.dayOfYear === i);
+      const intensity = activity ? activity.intensity : 20;
+      const backgroundColor = `rgba(0, 0, 0, ${intensity / 100})`;
+
+      elements.push(
+        <div
+          key={i}
+          className="h-4 w-4 flex-shrink-0 rounded-sm"
+          style={{ backgroundColor }}
+        />,
+      );
+    }
+    return elements;
+  };
 
   return (
     <div className="m-4 space-y-4">
-      {/* CANVAS */}
-      <div className="min-w-[300px] bg-white text-center shadow-lg sm:min-w-[800px]">
-        <svg
-          ref={svgRef}
-          width="100%"
-          height="400px"
-          viewBox={`0 0 ${SVG_WIDTH} ${SVG_HEIGHT}`}
-          preserveAspectRatio="xMidYMid meet"
-          className="gridBackground"
-        >
-          <rect
-            width={SVG_WIDTH}
-            height={SVG_HEIGHT}
-            fill={backgroundColor}
-            stroke={"#cfcfcf"}
-            strokeWidth={"2"}
-          />
-        </svg>
+      <div className="mx-auto flex flex-wrap justify-center gap-2 border bg-white p-4 text-center shadow-lg">
+        {generateElements()}
       </div>
     </div>
   );
