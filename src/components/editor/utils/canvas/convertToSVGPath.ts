67b73cdd613aa567b45ce7c07f16c57f9c431c09
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2023-12-11 21:50:22 +0100

    refactored

--- src/components/editor/utils/canvas/convertToSVGPath.ts
+++
@@ -1,8 +0,0 @@
-export function convertToSVGPath(coordinates: [number, number][]): string {
-  return coordinates
-    .map((coord, index) => {
-      const command = index === 0 ? "M" : "L";
-      return `${command} ${coord[0]} ${coord[1]}`;
-    })
-    .join(" ");
-}
