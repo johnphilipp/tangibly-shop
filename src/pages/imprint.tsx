7ae3d843ad9b49a1b168cf7145ba345bd28cca7a
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2023-12-12 01:01:31 +0100

    added imprint

---
+++ src/pages/imprint.tsx
@@ -0,0 +1,55 @@
+import Layout from "~/components/Layout";
+
+export default function Imprint() {
+  return (
+    <Layout>
+      <h1 className="mt-12 text-3xl">Privacy Policy</h1>
+      <p>
+        <h2 className="mt-4 text-2xl">Responsible entity</h2>
+        <br />
+        Johannes Wenz
+        <br />
+        Torstrasse 20
+        <br />
+        9000 St. Gallen
+        <br />
+        Switzerland
+        <br />
+        <strong>Email</strong>: contact@tangibly.me
+        <br />
+        <br />
+        <h2 className="mt-4 text-2xl">Disclaimer</h2>
+        <br />
+        The author assumes no liability for the correctness, accuracy,
+        timeliness, reliability and completeness of the information.
+        <br />
+        Liability claims against the author for material or immaterial damage
+        resulting from access to, use or non-use of the published information,
+        from misuse of the connection or from technical malfunctions are
+        excluded.
+        <br />
+        <br />
+        All offers are non-binding. The author expressly reserves the right to
+        change, add to, or delete parts of the pages or the entire offer without
+        prior notice, or to temporarily or permanently cease publication.
+        <br />
+        <br />
+        <h2 className="mt-4 text-2xl">Disclaimer for content and links</h2>
+        <br />
+        References and links to third party websites are outside our area of
+        responsibility. It rejected any responsibility for such websites. Access
+        to and use of such websites is at the user&apos;s own risk.
+        <br />
+        <br />
+        <h2 className="mt-4 text-2xl">Copyright declaration</h2>
+        <br />
+        The copyrights and all other rights to content, images, photos or other
+        files on this website belong exclusively to Johannes Wenz or the
+        specifically named rights holders. The written consent of the copyright
+        holder must be obtained in advance for the reproduction of any elements.
+      </p>
+      <br />
+      <br />
+    </Layout>
+  );
+}
