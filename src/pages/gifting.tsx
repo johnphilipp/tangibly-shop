789e92589cc7794f6ae995de15c34c135a25ba10
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2023-12-13 16:19:20 +0100

    handled unauthed state for pages

--- src/pages/gifting.tsx
+++ src/pages/gifting.tsx
@@ -1,5 +1,12 @@
+import { useSession } from "next-auth/react";
+import { LoadingLayout } from "~/components/Loading";
+import { LoginPrompt } from "~/components/LoginPrompt";
 import Gifting from "~/components/gifting";
 
 export default function GiftingPage() {
+  const { status } = useSession();
+
+  if (status === "loading") return <LoadingLayout />;
+  if (status === "unauthenticated") return <LoginPrompt />;
   return <Gifting />;
 }
--- src/pages/saved.tsx
+++ src/pages/saved.tsx
@@ -1,5 +1,12 @@
+import { useSession } from "next-auth/react";
+import { LoadingLayout } from "~/components/Loading";
+import { LoginPrompt } from "~/components/LoginPrompt";
 import Saved from "~/components/saved";
 
 export default function SavedPage() {
+  const { status } = useSession();
+
+  if (status === "loading") return <LoadingLayout />;
+  if (status === "unauthenticated") return <LoginPrompt />;
   return <Saved />;
 }
