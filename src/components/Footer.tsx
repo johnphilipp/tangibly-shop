cb91df1342acd1ec6b486c57667b82c0b99a611f
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2024-01-20 16:29:59 +0100

    added links to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,23 @@
+import Link from "next/link";
 import { Logo } from "./Logo";
 
 export default function Footer() {
   return (
-    <footer className="bottom-0 mx-auto max-w-7xl border-gray-900/10 py-4 sm:py-6">
+    <footer className="mx-auto max-w-7xl border-gray-900/10 py-4 sm:py-6">
       <div className="mb-4 border-t sm:mb-6" />
       <div className="flex justify-between px-4 sm:px-6">
         <p className="text-xs leading-5 text-gray-500">&copy; 2024 Tangibly</p>
         <div>
           <Logo />
         </div>
-        <p className="text-xs leading-5 text-gray-500">
-          Made with 💚 in St. Gallen
-        </p>
+        <div className="z-10 space-x-4 text-xs leading-5 text-gray-500 sm:space-x-6">
+          <Link href="/imprint" className="underline">
+            Imprint
+          </Link>
+          <Link href="/privacy-policy" className="underline">
+            Privacy Policy
+          </Link>
+        </div>
       </div>
     </footer>
   );
