05962a22d19d9fca6f34149013b9df01e3db6935
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2024-01-20 16:31:10 +0100

    changed button label to login with strava

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -84,7 +84,7 @@ export default function LandingPage() {
                         className="flex w-full items-center justify-center rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600 sm:w-[200px] lg:w-[250px]"
                       >
                         <BsStrava className="mr-2 inline-block h-5 w-5" />
-                        Connect to Strava
+                        Login with Strava
                       </Button>
                       <button
                         className="flex items-center justify-center rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:w-[200px] lg:w-[250px]"
