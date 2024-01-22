cf5bd6df9b953155a164dbbdc16612aef9cfa09e
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2024-01-22 15:06:18 +0100

    made footer sticky at bottom

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Logo } from "./Logo";
 
 export default function Footer() {
   return (
-    <footer className="mx-auto max-w-7xl border-gray-900/10 py-4 sm:py-6">
+    <footer className="sticky top-[100vh] mx-auto max-w-7xl border-gray-900/10 py-4 sm:py-6">
       <div className="mb-4 border-t sm:mb-6" />
       <div className="flex justify-between px-4 sm:px-6">
         <p className="text-xs leading-5 text-gray-500">&copy; 2024 Tangibly</p>
